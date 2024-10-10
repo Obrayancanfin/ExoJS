820f61353048d098399b6732dbdd5699980dada9
Canfin Obrayan <104076031+Obrayancanfin@users.noreply.github.com>
2024-10-10 09:15:30 +0200

    Add files via upload

---
+++ Docs/js/main.js
@@ -0,0 +1,57 @@
+let imgpkmn= document.querySelector("img.pokemon")
+let nbrpkmn=document.querySelector("input.nbrpkmn").getAttribute("value")
+let buttonNext=document.querySelector("button.NextBouton")
+let buttonPrevious=document.querySelector("button.PreviousBouton")
+let affichageNom =document.querySelector("p.Nom")
+let affichageType =document.querySelector("p.Type")
+let nompkmn ;
+let typeNom;
+let imageType;
+async function getData(nbrpkmn) {
+    const url = "https://tyradex.vercel.app/api/v1/pokemon/"+`${nbrpkmn}`;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        const json = await response.json();
+        nompkmn=json.name.en.toLowerCase();
+        imgpkmn.src="https://projectpokemon.org/images/normal-sprite/"+`${nompkmn}`+".gif"
+        nompkmn=json.name.fr
+        imgpkmn.setAttribute("alt",`${nompkmn}`)
+        affichageNom.textContent="Nom : "+`${nompkmn}`
+        affichageType.innerHTML = `Type : `
+        for (const type of json.types) {
+        imageType=type.image
+        typeNom=type.name
+        affichageType.innerHTML +=`<img src="${imageType}" alt="${typeNom}"> ${typeNom}`
+        }
+        affichageType.innerHTML +=`</p>`
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+data = getData(nbrpkmn);
+
+buttonNext.addEventListener("click",()=>{
+    if (nbrpkmn>1025){
+        buttonNext.setAttribute("disabled", false)
+    }else {
+        buttonNext.removeAttribute("disabled");
+        nbrpkmn++
+        console.log(nbrpkmn)
+        getData(nbrpkmn)
+    }
+})
+
+buttonPrevious.addEventListener("click",()=>{
+    if (nbrpkmn<=1){
+        buttonPrevious.setAttribute("disabled", false)
+    }else {
+        buttonPrevious.removeAttribute("disabled");
+        nbrpkmn--
+        console.log(nbrpkmn)
+        getData(nbrpkmn)
+    }
+})
